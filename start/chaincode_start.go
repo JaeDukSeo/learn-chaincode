da00b086fe9241580e737d885ec60a6cc2700de3
jdseo <jae.duk.seo@ryerson.ca>
2017-03-11 16:15:50 -0500

    first

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -191,8 +191,9 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 		return nil, errors.New(jsonResp)
 	}
+	jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 
-	return valAsbytes, nil
+	return jsonResp, nil
 }
 
 // Invoke isur entry point to invoke a chaincode function

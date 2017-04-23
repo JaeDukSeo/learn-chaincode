1c4a0adf980281ce0d1ec2b7f57ca9e521af7e97
jdseo <jae.duk.seo@ryerson.ca>
2017-04-22 21:07:23 -0400

    Apr 22 update 4

---
+++ practice.go
@@ -0,0 +1,15 @@
+// package main
+// import (
+// 	"fmt"
+// )
+
+// // 0. this is the main function of the go lang
+// func main() {
+
+
+
+//     fmt.Println("hello world")
+// }
+
+
+// // --- END OF THE CODE -----
